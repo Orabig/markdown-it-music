6eefea8eb27d71c31d5ca5ab8403be5b2b5c1eb3
Orabig <benoit.chauvet@gmail.com>
2021-02-02 22:28:35 +0100

    WIP : syntax samples

--- renderers/vexflow_renderer.js
+++ renderers/vexflow_renderer.js
@@ -5,68 +5,71 @@ function rendervexflow(str, opts) {
   // Parse the vexflow music notation
 
   const VF = Vex.Flow;
-  const div = document.createElement("div");
-  const renderer = new VF.Renderer(div, VF.Renderer.Backends.SVG);
+  const svgContainer = document.createElement("div");
+  //const renderer = new VF.Renderer(svgContainer, VF.Renderer.Backends.SVG);
 
-  // Configure the rendering context.
-  renderer.resize(500, 500);
-  const context = renderer.getContext();
-  context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+  var vf = new VF.Factory({
+    renderer: {elementId: svgContainer, width: 600, height: 400}
+  });
+  var score = vf.EasyScore();
+  var system = vf.System();
   
+  system.addStave({
+    voices: [
+      score.voice(
+        score.notes('C#5/q, B4[stem="down"], B4/8.',{stem: 'up'})
+        .concat(
+          vf.GraceNote(
+            {keys: ['d/4'], duration: '16', slash: true}
+          )
+        )
+        .concat(
+          score.tuplet(score.beam(
+            score.notes('B3/8, C#4/8, D4/8',{stem: 'up'})
+            )
+          )
+        )
+      ),
+      score.voice(score.notes('(C#4 E4 G4)/h, C4', {stem: 'down'}))
+    ]
+  }).addClef('treble').addTimeSignature('4/4')
+  .setText("Am   Cm       Bb7",VF.StaveText.Position.ABOVE,{}); // http://public.vexflow.com/build/docs/stavetext.html
+  
+  system.addStave({
+    voices: [
+      score.voice(score.notes('C#3/q, B2, A2/8, B2, C#3, D3', {clef: 'bass', stem: 'up'})),
+      score.voice(score.notes('C#2/h, C#2', {clef: 'bass', stem: 'down'}))
+    ]
+  }).addClef('bass').addTimeSignature('4/4');
+  
+  system.addConnector().setType(0); // http://public.vexflow.com/build/docs/staveconnector.html
+  system.addConnector().setType(1);
+  vf.draw();
+  return svgContainer.outerHTML;
+  // Configure the rendering context.
+//  
+//  const context = renderer.getContext();
+//  context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+/*
 // measure 1
 var staveMeasure1 = new Vex.Flow.Stave(10, 0, 300);
 staveMeasure1.addClef("treble").setContext(context).draw();
 
 var notesMeasure1 = [
-  new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" }),
-  new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "q" }),
-  new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "qr" }),
-  new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "q" }),
+  new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "q" })
 ];
 
 // Helper function to justify and draw a 4/4 voice
 Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure1, notesMeasure1);
 
-// measure 2 - juxtaposing second measure next to first measure
-var staveMeasure2 = new Vex.Flow.Stave(
-  staveMeasure1.width + staveMeasure1.x,
-  0,
-  400
-);
-staveMeasure2.setContext(context).draw();
 
-var notesMeasure2_part1 = [
-  new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "8" }),
-  new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "8" }),
-  new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "8" }),
-  new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "8" }),
-];
-
-var notesMeasure2_part2 = [
-  new Vex.Flow.StaveNote({ keys: ["c/4"], duration: "8" }),
-  new Vex.Flow.StaveNote({ keys: ["d/4"], duration: "8" }),
-  new Vex.Flow.StaveNote({ keys: ["b/4"], duration: "8" }),
-  new Vex.Flow.StaveNote({ keys: ["c/4", "e/4", "g/4"], duration: "8" }),
-];
-
-// create the beams for 8th notes in 2nd measure
-var beam1 = new Vex.Flow.Beam(notesMeasure2_part1);
-var beam2 = new Vex.Flow.Beam(notesMeasure2_part2);
-
-var notesMeasure2 = notesMeasure2_part1.concat(notesMeasure2_part2);
-
-Vex.Flow.Formatter.FormatAndDraw(context, staveMeasure2, notesMeasure2);
-
-// Render beams
-beam1.setContext(context).draw();
-beam2.setContext(context).draw();
-  // Format and justify the notes to 400 pixels.
+// Format and justify the notes to 400 pixels.
 //  var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
   
   // Render voice
   //voice.draw(context, stave);
 
-  return div.outerHTML;
+  return svgContainer.outerHTML;*/
 }
 
 module.exports = {
